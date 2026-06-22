ba4f9dee0c0efab20ca964c29b552feb19b4bf7c
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-06-22 14:58:46 -0300

    Arquivo principal

---
+++ main.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import cv2
+from src.correlacao_2d import correlacao_gray 
+
+def main ():
+    img = cv2.imread("download.jpeg")
+
+    gx = correlacao_gray("download.jpeg", "sobel_x.txt")
+
+    plt.imshow(gx, cmap='gray')
+    plt.show()
+
+if(__name__ == "__main__"):
+    main()
