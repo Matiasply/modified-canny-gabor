2859a7c2f2d7db4e3b762dd5916488b3ed327710
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-06-23 15:03:32 -0300

    src/gerador_filtros.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
 import cv2
 from src.correlacao_2d import *
+from src.gerador_filtros import *
 
 def main ():
     img = cv2.imread("download.jpeg")
     kernel = carregar_matriz("filtros/sobel_x.txt")
     gx = correlacao_rgb(img, kernel)
 
-    plt.imshow(np.clip(gx, 0, 255).astype(np.uint8))
+    parametros_gabor = ler_parametros("config/gabor.json")
+
+    filtros = gerador_parametrico(parametros_gabor)
+
+    plt.imshow(filtros[0], cmap='gray')
+
     plt.show()
 
 if(__name__ == "__main__"):
