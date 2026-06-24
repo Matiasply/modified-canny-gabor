69889652a286354210e7cedd1ab498ea0f183a73
Dennynson <dennynson.scheydt@academico.ufpb.br>
2026-06-24 16:42:49 -0300

    Integrando Etapa de Magnitude Combinada ao Fluxo Principal da Main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 from src.correlacao_2d import *
 from src.gerador_filtros import *
 from src.filtragem_orientada import *
+from src.magnitude import *
 
 def main ():
     img = cv2.imread("download.jpeg")
@@ -13,6 +14,15 @@ def main ():
 
     filtros = gerador_parametrico(parametros_gabor)
     filtragem = filtro_orientado(img, filtros)
-    
+    magitudes = magnitude_combinada(filtragem)
+
+    for angulo, filtro in magitudes.items():
+        plt.subplot(2, 4, int(angulo/22.5)+1)
+        plt.imshow(filtro, cmap='gray')
+        plt.title(f"{angulo}°")
+        plt.axis('off')
+
+    plt.show()
+
 if(__name__ == "__main__"):
     main()
