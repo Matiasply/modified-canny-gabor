dee4c693078a4613be8e111d73c6f82801d472e1
Erick Batista <erick.batista@academico.ufpb.br>
2026-06-26 13:01:56 -0300

    Script para obter as imagens dos experimentos

--- main.py
+++ main.py
@@ -1,33 +1,99 @@
 import matplotlib.pyplot as plt
 import cv2
+from pathlib import Path
 from src.correlacao_2d import *
-from src.gerador_filtros import *
+from src.gerador_filtros_gabor import *
 from src.filtragem_orientada import *
 from src.magnitude import *
 from src.nms import *
 from src.histerese import *
+from src.sobel import *
+from src.expansao_histograma import *
 
 def main ():
-    img = cv2.imread("Imagens/download.jpeg")
+    pasta = Path('./imagens_fonte')
 
-    parametros_gabor = ler_parametros("config/gabor.json")
+    for arquivo in pasta.iterdir():
+        if arquivo.is_file():
+            img = cv2.imread(str(arquivo))
 
-    filtros = gerador_parametrico(parametros_gabor)
-    filtragem = filtro_orientado(img, filtros)
-    magnitudes = magnitude_combinada(filtragem)
-    orientacao, magnitude_final = magnitude_maxima(magnitudes)
-    nms = non_maximum_suppression(orientacao, magnitude_final)
-    max_val = nms.max()
+            # Canny tradicional
+            kernel_gaussiano = carregar_matriz("filtros/gaussiano_5x5.txt")
+            imagem_suavizada_gray = correlacao_gray(img, kernel_gaussiano)
+            magnitudes, orientacoes = magnitude_orientada_sobel(imagem_suavizada_gray)
 
-    Thigh = max_val * 0.2
-    Tlow = max_val * 0.1
+            # Imagem das magnitudes
+            imagem_magnitudes = expandir_histograma(magnitudes)
+            plt.imshow(imagem_magnitudes, cmap='gray')
+            plt.axis('off')
+            plt.savefig(f'./canny_tradicional/magnitude/{arquivo.stem}_MagTradicional.png', bbox_inches='tight', pad_inches=0)
+            plt.close()
 
-    histerese_resultado = histerese(nms, Tlow=Tlow, Thigh=Thigh)
+            nms = non_maximum_suppression(orientacoes, magnitudes)
 
-    plt.imshow(histerese_resultado, cmap='gray')
-    plt.title("Resultado final - Histerese (Canny)")
-    plt.axis('off')
-    plt.show()
+            # Imagem do NMS
+            imagem_nms = expandir_histograma(nms)
+            plt.imshow(imagem_nms, cmap='gray')
+            plt.axis('off')
+            plt.savefig(f'./canny_tradicional/nms/{arquivo.stem}_NMSTradicional.png', bbox_inches='tight', pad_inches=0)
+            plt.close()
+
+            max_val = nms.max()
+            Thigh = max_val * 0.2
+            Tlow = max_val * 0.1
+
+            resultado = histerese(nms, Tlow=Tlow, Thigh=Thigh)
+            plt.imshow(resultado, cmap='gray')
+            plt.axis('off')
+            plt.savefig(f'./canny_tradicional/resultado/{arquivo.stem}_ResultadoTradicional.png', bbox_inches='tight', pad_inches=0)
+            plt.close()
+                                      
+            # Canny modificado
+            pasta_parametros = Path('./filtros_gabor_teste')
+            for filtro in pasta_parametros.iterdir():
+                if filtro.is_file() and filtro.suffix == '.json':
+                    parametros_gabor = ler_parametros(filtro)
+                    filtros_gabor = gerar_filtros_gabor_orientados(parametros_gabor)
+                    filtragem = correlacao_orientada(img, filtros_gabor)
+                    magnitudes = magnitude_combinada(filtragem)
+
+                    # Imagem das magnitudes orientadas
+                    for i in magnitudes.keys():
+                        imagem_magnitudes = expandir_histograma(magnitudes[i])
+                        plt.imshow(imagem_magnitudes, cmap='gray')
+                        plt.axis('off')
+                        plt.savefig(f'./canny_modificado/magnitude/{arquivo.stem}_{filtro.stem}_Mag{i}Modificado.png', bbox_inches='tight', pad_inches=0)
+                        plt.close()
+
+                    orientacao_final, magnitude_final = magnitude_maxima_gabor(magnitudes)
+                    
+                    # Imagem das magnitudes finais
+                    imagem_magnitudes_final = expandir_histograma(magnitude_final)
+                    plt.imshow(imagem_magnitudes_final, cmap='gray')
+                    plt.axis('off')
+                    plt.savefig(f'./canny_modificado/magnitude/{arquivo.stem}_{filtro.stem}_MagFinalModificado.png', bbox_inches='tight', pad_inches=0)
+                    plt.close()
+
+                    nms = non_maximum_suppression(orientacao_final, magnitude_final)
+
+                    # Imagem do NMS
+                    imagem_nms = expandir_histograma(nms)
+                    plt.imshow(imagem_nms, cmap='gray')
+                    plt.axis('off')
+                    plt.savefig(f'./canny_modificado/nms/{arquivo.stem}_{filtro.stem}_NMSModificado.png', bbox_inches='tight', pad_inches=0)
+                    plt.close()
+
+                    max_val = nms.max()
+                    Thigh = max_val * 0.2
+                    Tlow = max_val * 0.1
+
+                    resultado = histerese(nms, Tlow=Tlow, Thigh=Thigh)
+
+                    plt.imshow(resultado, cmap='gray')
+                    plt.axis('off')
+                    plt.savefig(f'./canny_modificado/resultado/{arquivo.stem}_{filtro.stem}_ResultadoModificado.png', bbox_inches='tight', pad_inches=0)
+                    plt.close()
+
 
 if(__name__ == "__main__"):
     main()
